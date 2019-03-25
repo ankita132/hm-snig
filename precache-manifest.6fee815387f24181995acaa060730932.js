e73d2ecaad41e25666878ebad91b572c5b9bf170
ankita132 <2ankitasahoo13@gmail.com>
2019-03-25 13:00:44 +0530

    Updates

---
+++ precache-manifest.6fee815387f24181995acaa060730932.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "894c7a65dd2669f0f578",
+    "url": "/nehru-hall/static/js/runtime~main.62e50522.js"
+  },
+  {
+    "revision": "c8e95875ce0a6714b65a",
+    "url": "/nehru-hall/static/js/main.5a64ae33.chunk.js"
+  },
+  {
+    "revision": "eb1f17c42603d6b688a7",
+    "url": "/nehru-hall/static/js/2.e6d8fc6b.chunk.js"
+  },
+  {
+    "revision": "c8e95875ce0a6714b65a",
+    "url": "/nehru-hall/static/css/main.e34c3abf.chunk.css"
+  },
+  {
+    "revision": "eb1f17c42603d6b688a7",
+    "url": "/nehru-hall/static/css/2.f359485a.chunk.css"
+  },
+  {
+    "revision": "5ec56edcd28d2ac5a4c8a9937a1ba26e",
+    "url": "/nehru-hall/index.html"
+  }
+];
