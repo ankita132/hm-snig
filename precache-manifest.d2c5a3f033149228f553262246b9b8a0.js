528cad8020ab7632f12c65ccc017b6b9a3f9a521
ankita132 <2ankitasahoo13@gmail.com>
2019-03-27 20:12:27 +0530

    Updates

--- precache-manifest.d2c5a3f033149228f553262246b9b8a0.js
+++ precache-manifest.d2c5a3f033149228f553262246b9b8a0.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/nehru-hall/static/js/runtime~main.62e50522.js"
   },
   {
-    "revision": "e61e00f8e1bfe1d76eff",
-    "url": "/nehru-hall/static/js/main.4d10e97e.chunk.js"
+    "revision": "9dbc78fd6777fb64b872",
+    "url": "/nehru-hall/static/js/main.3d28e133.chunk.js"
   },
   {
     "revision": "eb1a5d0f4331416a73ad",
     "url": "/nehru-hall/static/js/2.e697a78a.chunk.js"
   },
   {
-    "revision": "e61e00f8e1bfe1d76eff",
-    "url": "/nehru-hall/static/css/main.e34c3abf.chunk.css"
+    "revision": "9dbc78fd6777fb64b872",
+    "url": "/nehru-hall/static/css/main.a250148d.chunk.css"
   },
   {
     "revision": "eb1a5d0f4331416a73ad",
     "url": "/nehru-hall/static/css/2.f359485a.chunk.css"
   },
   {
-    "revision": "00957365d67d8a88e9b29c394ef73d35",
+    "revision": "fb24da18cee80e0299bafaf0bb2d1b2b",
     "url": "/nehru-hall/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/nehru-hall/precache-manifest.799decc818e6593280611cea628c30dc.js"
+  "/nehru-hall/precache-manifest.d2c5a3f033149228f553262246b9b8a0.js"
 );
 
 workbox.clientsClaim();
